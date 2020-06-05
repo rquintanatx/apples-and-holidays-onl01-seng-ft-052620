9551d9c956e5359f79a23fe7a83bdd862ba8edc9
Robert Quintana <robertquintanatx@gmail.com>
2020-06-05 08:32:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -84,7 +84,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, season_holiday_hash|
+    season_string = season.to_s  
+    
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
