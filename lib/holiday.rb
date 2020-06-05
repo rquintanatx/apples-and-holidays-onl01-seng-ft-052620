8f4fe4f90524da8340fc2fb38a4b43ed0fc8849b
Robert Quintana <robertquintanatx@gmail.com>
2020-06-05 07:50:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,10 +47,11 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
+  winter_supply_array = []
   holiday_hash.each do |season, season_holiday_hash|
     if season == :winter
       season_holiday_hash.each do |holiday, supply_array|
-        
+        winter_supply_array << supply_array
       end  
     end 
   end   
