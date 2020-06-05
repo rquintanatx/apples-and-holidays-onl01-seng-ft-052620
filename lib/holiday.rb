f71f1e155c25e93018dd8298b8af80550c6384ae
Robert Quintana <robertquintanatx@gmail.com>
2020-06-05 08:56:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,7 +89,6 @@ def all_supplies_in_holidays(holiday_hash)
     season_holiday_hash.each do |holiday, supply_array|
       holiday_string = holiday.to_s
       holiday_string_array = holiday_string.split(/_/)
-      binding.pry
       holiday_string_array.each do |word| 
         word.capitalize!
       end
